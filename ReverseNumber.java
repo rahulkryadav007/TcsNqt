81f0e6d93f4c259cbeba47b7c3dd514f7202c5dc
unknown <ry67272925@gmail.com>
2026-03-30 08:19:10 +0530

    Add ReverseNumber

---
+++ ReverseNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter a number");
+       int n = sc.nextInt();
+       int rev = 0;
+       
+       while(n > 0) {
+    	   rev =  rev * 10 + n % 10;
+    	   n = n/10;
+       }
+       System.out.println(rev);
+	}
+
+}
